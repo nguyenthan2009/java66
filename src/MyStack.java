9b74e8d642709a1c7fa299cd635b4859189afb21
unknown <sauvole75@gmail.com>
2021-07-15 20:19:14 +0700

    'add'

---
+++ src/MyStack.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class MyStack {
+    public static void main(String[] args) {
+        int[] Array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Stack<Integer> Stack = new Stack<>();
+        for (int i = 0; i < Array.length; i++) {
+            System.out.println(Stack.push(Array[i]));
+        }
+        for (int i = 0; i < Stack.size() + Array.length; i++) {
+            Array[i] = Stack.pop();
+            System.out.println(Array[i]);
+        }
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Nhập chuỗi muốn đảo ngược");
+        String chuoi = Sc.nextLine();
+        String[] Array1 = chuoi.split("");
+        Stack<String> Stack1 = new Stack<>();
+        for (int i = 0; i < Array1.length; i++) {
+            System.out.println(Stack1.push(Array1[i]));
+        }
+        String str = "";
+        for (int i = 0; i < Stack1.size() + Array1.length; i++) {
+            Array1[i] = Stack1.pop();
+            str += Array1[i];
+        }
+        System.out.println("chuỗi sau khi đảo nguoc là"+str);
+    }
+}
